b52e81d86df34e8dbf2343c5e0d8097514b0fb51
Cedric <crubbertire@rogers.com>
2025-12-11 15:39:52 -0500

    query functionality

--- app.js
+++ app.js
@@ -1,27 +1,67 @@
+const apiUrl = "https://pokeapi.co/api/v2/pokemon"
 let page = 0;
 
-const fetchPokedex = () => {
-  $.get(`https://pokeapi.co/api/v2/pokemon?offset=${page}`, function (data, status) {
-    for (rootPokemon of data.results) {
 
-      $.get(`${rootPokemon.url}`, function (pokemon, status) {
-        console.log(pokemon)
-        const pokedexCard = `
-          <li class="pokedex-card" id="${pokemon.id}">
-            <img src="${pokemon.sprites.front_default}" alt="${pokemon.name} sprite" class="pokedex-sprite">
-            <p class="text-small">N°${pokemon.id}</p>
-            <h3>${pokemon.name}</h3>
-            <div class="type-container">
-            </div>
-          </li>
+// Placeholder card to order fetched pokemon data
+const generatePlaceholders = (id) => {
+  const placeholderCard = `<li class="pokedex-card" id="${id}"></li`
+  $("#pokedex-collection").append(placeholderCard)
+}
+
+
+// Using fetched data, inserting data into placeholder pokedex card
+const constructPokemonCard = (pokemon) => {
+  const pokedexCard = `
+          <img src="${pokemon.sprites.front_default}" alt="${pokemon.name} sprite" class="pokedex-sprite">
+          <p class="text-small">N°${pokemon.id}</p>
+          <h3>${pokemon.name}</h3>
+          <div class="type-container">
+          </div>
         `;
-        $("#pokedex-collection").append(pokedexCard)
-        pokemon.types.forEach(type => {
-          const name = type.type.name
-          const typeElem = `<p class="${name}">${name}</p>`;
-          $(`#${pokemon.id} .type-container`).append(typeElem)
-        }) 
-      })
+  $(`#${pokemon.id}`).html(pokedexCard)
+  pokemon.types.forEach(type => {
+    const name = type.type.name
+    const typeElem = `<p class="${name}">${name}</p>`;
+    $(`#${pokemon.id} .type-container`).append(typeElem)
+  })
+}
+
+// Generate 20 pokedex cards for pokedex page
+const generateManyPokedexEntries = (data) => {
+  // Generate placeholders
+  data.results.forEach(pokemon => {
+    const id = pokemon.url.replace("https://pokeapi.co/api/v2/pokemon/", "").replace("/", "")
+    generatePlaceholders(id)
+  })
+
+  // Populate placeholders
+  data.results.forEach(async (rootPokemon) => {
+    $.get(`${rootPokemon.url}`, function (pokemon, status) {
+      constructPokemonCard(pokemon)
+    })
+  })
+}
+
+
+// Fetches several pokedex entries
+const fetchManyPokemon = () => {
+  $.get(`${apiUrl}?offset=${page}`, function (data, status) {
+    generateManyPokedexEntries(data)
+  })
+}
+
+
+
+const queryPokemon = (searchInput) => {
+  $.get(`${apiUrl}/${searchInput}`, function(data, status) {
+    $("#pokedex-collection").empty()
+    page = 0;
+
+    if ("results" in data) { // 'pokemon/' queried
+      generateManyPokedexEntries(data)
+    } else {  // 'pokemon/valid-entry' queried
+      generatePlaceholders(data.id)
+      constructPokemonCard(data)
     }
   })
 }
@@ -30,14 +70,10 @@ const fetchPokedex = () => {
 
 
 
-
-
-
-
-
 $(function() {
 
-  $("#pokedex-collection").on("load", fetchPokedex())
-  
-
+  $("#pokedex-collection").on("load", fetchManyPokemon())
+  $("#search-button").click(function () {
+    queryPokemon($("#search-input").val())
+  })
 })
